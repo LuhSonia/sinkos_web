7d00a5751e345f342df8f085f85c08d892405f51
sonia sintiari <soniasintiari13@gmail.com>
2024-01-20 09:59:21 +0800

    first commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,19 @@
+<?php
+// app/Http/Controllers/HomeController.php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+
+    public function index()
+    {
+        return view('home.userpage');
+    }
+    public function redirect()
+    {
+        return view('admin.home');
+    }
+}
